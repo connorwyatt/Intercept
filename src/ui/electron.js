b6c837d2d7c66a89cfc67e9884902682421e67bf
Connor Wyatt <connorwyatt1@gmail.com>
2016-04-11 21:59:21 +0100

    Electron window now opens as 1200 x 900

--- src/ui/electron.js
+++ src/ui/electron.js
@@ -15,7 +15,7 @@ app.on('window-all-closed', function() {
 
 function startElectronApp(port) {
   app.on('ready', function() {
-    mainWindow = new BrowserWindow({ width: 800, height: 600 });
+    mainWindow = new BrowserWindow({ width: 1200, height: 900 });
 
     let uiUrl = url.format({
       protocol: 'http',
