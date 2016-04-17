da533acf71fa715908932e0aec9f3849f496bcc1
Connor Wyatt <connorwyatt1@gmail.com>
2016-04-17 18:30:01 +0100

    Updated information on dashboard to be dynamic

--- src/ui/app/areas/InDashboard/InDashboard.ts
+++ src/ui/app/areas/InDashboard/InDashboard.ts
@@ -1,5 +1,6 @@
-import { Component, ViewEncapsulation } from 'angular2/core';
+import { Component, ViewEncapsulation, OnInit } from 'angular2/core';
 import { InRequestsList } from '../../components/InRequestsList/InRequestsList.component';
+import { InHttp } from '../../services/InHttp';
 import { InRequestsHelper } from '../../services/InRequestsHelper';
 
 declare const __moduleName: String;
@@ -16,14 +17,46 @@ declare const __moduleName: String;
   encapsulation: ViewEncapsulation.Native,
   directives: [[InRequestsList]]
 })
-export class InDashboard {
+export class InDashboard implements OnInit {
   private requestsHelper: InRequestsHelper;
+  private http: InHttp;
+  private proxySettings: Object;
+  private proxySettingsResolved: boolean;
+  private targetHostSettings: Object;
+  private targetHostSettingsResolved: boolean;
 
   private get requests() {
     return this.requestsHelper.getRequests();
   }
 
-  constructor(requestsHelper: InRequestsHelper) {
+  constructor(requestsHelper: InRequestsHelper,
+              http: InHttp) {
     this.requestsHelper = requestsHelper;
+    this.http = http;
+  }
+
+  ngOnInit() {
+    this.getProxySettings();
+    this.getTargetHostSettings();
+  }
+
+  private getProxySettings() {
+    this.http.get('/settings/proxy')
+      .subscribe((data) => {
+        this.proxySettings = data.data.ProxySettings;
+        this.proxySettingsResolved = true;
+      }, (error) => {
+        console.error(error);
+      });
+  }
+
+  private getTargetHostSettings() {
+    this.http.get('/settings/targetHost')
+      .subscribe((data) => {
+        this.targetHostSettings = data.data.TargetHostSettings;
+        this.targetHostSettingsResolved = true;
+      }, (error) => {
+        console.error(error);
+      });
   }
 }
