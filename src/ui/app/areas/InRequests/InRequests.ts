45ff08b7d9d4706b855d3517975dca8536173f48
Connor Wyatt <connorwyatt1@gmail.com>
2016-04-18 00:23:46 +0100

    Added default message to tables

--- src/ui/app/areas/InRequests/InRequests.ts
+++ src/ui/app/areas/InRequests/InRequests.ts
@@ -1,7 +1,10 @@
 import { Component, ViewEncapsulation } from 'angular2/core';
 import { InCard } from '../../components/InCard/InCard.component';
-import { InRequestsList } from '../../components/InRequestsList/InRequestsList.component';
+import { InTable } from '../../components/InTable/InTable.component';
 import { InRequestsHelper } from '../../services/InRequestsHelper';
+import { InReversePipe } from '../../pipes/InReverse.pipe';
+import { IInTableField } from '../../components/InTable/IInTableField';
+import { IInRequest } from '../../interfaces/IInRequest';
 
 declare const __moduleName: string;
 
@@ -12,19 +15,37 @@ declare const __moduleName: string;
   styleUrls: [
     '../../styles/core.css',
     '../../components/InCard/InCard.css',
-    '../../components/InGrid/InGrid.css'
+    '../../components/InGrid/InGrid.css',
+    'InRequests.css'
   ],
   encapsulation: ViewEncapsulation.Native,
-  directives: [[InCard, InRequestsList]]
+  directives: [[InCard, InTable]],
+  pipes: [InReversePipe]
 })
 export class InRequests {
   private requestsHelper: InRequestsHelper;
+  private requestsFields: Array<IInTableField> = [
+    { fieldname: 'timestamp', label: 'Time' },
+    { fieldname: 'method', label: 'Method' },
+    { fieldname: 'statusCode', label: 'Status Code' },
+    { fieldname: 'url', label: 'URL' },
+    { fieldname: 'latency', label: 'Latency' }
+  ];
 
-  private get requests() {
+
+  private get requests(): Array<IInRequest> {
     return this.requestsHelper.getRequests();
   }
 
   constructor(requestsHelper: InRequestsHelper) {
     this.requestsHelper = requestsHelper;
+  }
+
+  private requestsRowClass(model: Object): string {
+    if (model.statusCode >= 400 && model.statusCode < 600) {
+      return 'negative';
+    } else {
+      return 'positive';
+    }
   }
 }
