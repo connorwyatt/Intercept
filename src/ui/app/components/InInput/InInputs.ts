a46dda848c81d2c2841b2d05c6b655b35d4f09aa
Connor Wyatt <connorwyatt1@gmail.com>
2016-04-24 01:30:28 +0100

    Fixed textarea import

--- src/ui/app/components/InInput/InInputs.ts
+++ src/ui/app/components/InInput/InInputs.ts
@@ -2,7 +2,7 @@ import { CONST_EXPR } from 'angular2/src/facade/lang';
 import { InInputText } from './InInputText/InInputText.component';
 import { InInputNumber } from './InInputNumber/InInputNumber.component';
 import { InInputSelect } from './InInputSelect/InInputSelect.component';
-import { InInputTextarea } from './InInputTextarea/InInputTextarea.component.ts';
+import { InInputTextarea } from './InInputTextarea/InInputTextarea.component';
 
 export const IN_INPUTS = CONST_EXPR([
   InInputText,
