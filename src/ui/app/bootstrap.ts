6c98bf0016ac454d09756f5953ceda4884c479c2
Connor Wyatt <connorwyatt1@gmail.com>
2016-05-01 11:13:02 +0100

    Angular upgrade to beta 17

--- src/ui/app/bootstrap.ts
+++ src/ui/app/bootstrap.ts
@@ -1,15 +1,16 @@
 import { bootstrap } from 'angular2/platform/browser';
 import { provide } from 'angular2/core';
 import { HTTP_PROVIDERS } from 'angular2/http';
-import { APP_BASE_HREF, ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy } from 'angular2/router';
+import { ROUTER_PROVIDERS } from 'angular2/router';
+import { APP_BASE_HREF, LocationStrategy, HashLocationStrategy } from 'angular2/platform/common';
 import { InApp } from './InApp';
 import { InHttp } from './services/InHttp';
 import { InElectronCommunication } from './services/InElectronCommunication';
 import { InRequestsHelper } from './services/InRequestsHelper';
 import { InMessagesHelper } from './services/InMessagesHelper';
+import { ElectronIPC } from './services/ElectronIpc';
 import 'es6-shim';
 import 'rxjs/Rx';
-import { ElectronIPC } from './services/ElectronIpc';
 
 declare const ELECTRON_IPC: ElectronIPC;
 
