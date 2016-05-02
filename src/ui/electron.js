a93d0640605b878f2a2adfa439ceea4c69e7c948
Connor Wyatt <connorwyatt1@gmail.com>
2016-05-02 13:42:07 +0100

    Added system shortcuts, closes #41

--- src/ui/electron.js
+++ src/ui/electron.js
@@ -46,9 +46,6 @@ function startElectronApp(port) {
       ]}, {
       label: "Edit",
       submenu: [
-        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-        { type: "separator" },
         { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
         { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
         { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
