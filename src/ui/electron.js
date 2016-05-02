4b5811d8b39f9378d3fcfb59a705c1c0fb723ef6
Connor Wyatt <connorwyatt1@gmail.com>
2016-05-02 13:41:55 +0100

    Added system shortcuts, closes #41

--- src/ui/electron.js
+++ src/ui/electron.js
@@ -36,6 +36,27 @@ function startElectronApp(port) {
     windows.mainWindow.on('closed', function() {
       windows.mainWindow = null;
     });
+
+    let menuTemplate = [{
+      label: "Application",
+      submenu: [
+        { label: `About ${app.getName()}`, selector: "orderFrontStandardAboutPanel:" },
+        { type: "separator" },
+        { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+      ]}, {
+      label: "Edit",
+      submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+      ]}
+    ];
+
+    electron.Menu.setApplicationMenu(electron.Menu.buildFromTemplate(menuTemplate));
   });
 }
 
