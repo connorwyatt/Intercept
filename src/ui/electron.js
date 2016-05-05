851a5bdbf5a5637646b1b6cc85a157e4d08c7e39
Connor Wyatt <connorwyatt1@gmail.com>
2016-05-05 21:35:55 +0100

    Refactored electron menu object

--- src/ui/electron.js
+++ src/ui/electron.js
@@ -37,20 +37,23 @@ function startElectronApp(port) {
       windows.mainWindow = null;
     });
 
-    let menuTemplate = [{
-      label: "Application",
-      submenu: [
-        { label: `About ${app.getName()}`, selector: "orderFrontStandardAboutPanel:" },
-        { type: "separator" },
-        { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
-      ]}, {
-      label: "Edit",
-      submenu: [
-        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-      ]}
+    let menuTemplate = [
+      {
+        label: 'Application',
+        submenu: [
+          { label: `About ${app.getName()}`, selector: 'orderFrontStandardAboutPanel:' },
+          { type: 'separator' },
+          { label: 'Quit', accelerator: 'Command+Q', click: () => app.quit() }
+        ]
+      }, {
+        label: 'Edit',
+        submenu: [
+          { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+          { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+          { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+          { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
+        ]
+      }
     ];
 
     electron.Menu.setApplicationMenu(electron.Menu.buildFromTemplate(menuTemplate));
