a8dca72c904317f17ee70769204fe0c750be596c
Connor Wyatt <connorwyatt1@gmail.com>
2016-05-21 09:11:51 +0100

    Updated to Electron v1.1.0

--- src/node/api/apiServer.js
+++ src/node/api/apiServer.js
@@ -3,7 +3,8 @@
 const http = require('http'),
   express = require('express'),
   expressApp = require('./expressApp'),
-  logger = require('../shared/services/logger');
+  logger = require('../shared/services/logger'),
+  getPort = require('get-port');
 
 let server = http.createServer(expressApp);
 
@@ -11,23 +12,27 @@ let port;
 
 if (process.env.NO_ELECTRON == 'true') {
   port = 6060;
+} else {
+  port = getPort();
 }
 
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+Promise.resolve(port).then((selectedPort) => {
+  server.listen(selectedPort);
+  server.on('error', onError);
+  server.on('listening', onListening);
 
-function onError(error) {
-  logger.error(error);
-  throw error;
-}
-
-function onListening() {
-  let address = server.address();
-
-  logger.info(`API server listening on port ${address.port}`);
-  
-  if (process.env.NO_ELECTRON !== 'true') {
-    require('../../ui/electron').startElectronApp(address.port);
+  function onError(error) {
+    logger.error(error);
+    throw error;
   }
-}
+
+  function onListening() {
+    let address = server.address();
+
+    logger.info(`API server listening on port ${address.port}`);
+
+    if (process.env.NO_ELECTRON !== 'true') {
+      require('../../ui/electron').startElectronApp(address.port);
+    }
+  }
+});
