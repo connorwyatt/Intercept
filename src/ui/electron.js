76a3e0c76bd39ac121074d1a187b7b8ce023c4c5
Connor Wyatt <connorwyatt1@gmail.com>
2016-06-12 16:37:04 +0100

    Fixed issue where selectAll wouldn't work

--- src/ui/electron.js
+++ src/ui/electron.js
@@ -49,13 +49,13 @@ function startElectronApp(port) {
         label: 'Edit',
         role: 'edit',
         submenu: [
-          { label: 'Undo', accelerator: 'CmdOrCtrl+Z', role: 'undo' },
-          { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', role: 'redo' },
+          { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+          { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
           { type: 'separator' },
-          { label: 'Cut', accelerator: 'CmdOrCtrl+X', role: 'cut' },
-          { label: 'Copy', accelerator: 'CmdOrCtrl+C', role: 'copy' },
-          { label: 'Paste', accelerator: 'CmdOrCtrl+V', role: 'paste' },
-          { label: 'Select All', accelerator: 'CmdOrCtrl+A', role: 'selectAll' }
+          { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+          { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+          { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+          { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
         ]
       },
       {
